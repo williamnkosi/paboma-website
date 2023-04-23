f7720781cfa15ad859be720bd4eb73fc8f2e8a6f
William Nkosi <william.dev.acc@gmail.com>
2023-04-22 23:51:37 -0400

    setup-nav-bar | Added ChakraUI to the project

---
+++ components/navbar/nav-bar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+type Props = {};
+
+const NavBar = (props: Props) => {
+  return (
+    <Box bg="blue" w="100%" p={4} color="white">
+      This is the Box
+    </Box>
+  );
+};
+
+export default NavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import NavBar from "@/components/navbar/nav-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const testing = "testing";
   return (
-    <>
-      <Navbar />
-    </>
+    <ChakraProvider>
+      <NavBar />
+    </ChakraProvider>
   );
 }
