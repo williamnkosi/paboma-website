76ba0a8916a027dd06fe89acf8ae30ae8ff57bd7
williamnkosi <william.dev.acc@gmail.com>
2023-05-04 14:34:10 -0400

    setup-auth-page | made changes the the signup page ui

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -11,13 +11,25 @@ const Signup = (props: Props) => {
               Signup
             </Text>
             <VStack spacing={4}>
-              <FormControl id="name">
-                <FormLabel>Name</FormLabel>
-                <Input type="text" placeholder="Enter name" />
+              <FormControl id="firstName">
+                <FormLabel>First Name</FormLabel>
+                <Input type="text" placeholder="Enter your first name." />
+              </FormControl>
+              <FormControl id="lastName">
+                <FormLabel>Last Name</FormLabel>
+                <Input type="text" placeholder="Enter your last name." />
+              </FormControl>
+              <FormControl id="userName">
+                <FormLabel>User name</FormLabel>
+                <Input type="text" placeholder="Enter a user name" />
+              </FormControl>
+              <FormControl id="phoneNumber">
+                <FormLabel>Phone Number</FormLabel>
+                <Input type="number" placeholder="Enter your phone number" />
               </FormControl>
               <FormControl id="email">
                 <FormLabel>Email address</FormLabel>
-                <Input type="email" placeholder="Enter email address" />
+                <Input type="email" placeholder="Enter a unique email address" />
               </FormControl>
               <FormControl id="password">
                 <FormLabel>Password</FormLabel>
